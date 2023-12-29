9b34bcefc2beab2038473981b7b182951b53674c
EmanuelOccelli <emanueloccelli@gmail.com>
2023-12-29 16:38:09 +0100

    2.2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,53 @@
+import "./Contact.css"
+
 const Contact = () => {
-    return <h1>Lista prof</h1>;
+  let domande=[]
+  let prof=[]
+  let i=0
+  fetch('https://raw.githubusercontent.com/1Lg20/ValutazioneDocenti/main/ProfJSON.json') 
+      .then(response => response.json())
+      .then(result => {
+        result.forEach(element=>{
+          prof.push(element)
+        })
+        console.log(result)
+
+        console.log("lista prof: " + prof[i].nome)
+        
+      })
+  fetch('https://raw.githubusercontent.com/1Lg20/ValutazioneDocenti/main/domandeProf.json') 
+    .then(response => response.json())
+    .then(result => {
+      result.forEach(element => {
+        domande.push(element)
+      });
+      //console.log(result)
+      //console.log("domande" + domande)
+    })
+    const caricalista=()=>{
+      console.log("CARICA LISTA")
+     for(i=0;i<prof.length;i++){
+      console.log(prof[i].nome)
+      console.log(i)
+      return(<div class="col-12 entra bordo">{prof[i].nome}</div>)
+     }
+    }
+    
+    return (
+      <div class="container bordo centra">
+        <div class="row spazia bordo">
+          <div class="col-8 centra bordo">
+            <input type="button" onClick={caricalista} value="CARICALISTA"></input>
+          </div>
+          <div class="col-4 centra bordo">
+            <div class="row centra bordo" >
+              {caricalista}
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    );
   };
   
   export default Contact;
